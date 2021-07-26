3d3e698380c578c6fa0ff922c0591d3f88feda3d
aysesinemozturk <aysesinemozturk@gmail.com>
2021-07-27 01:07:26 +0300

    car java file is coded.

---
+++ CarJava.java
@@ -0,0 +1,46 @@
+package JavaToPython;
+
+public class CarJava {
+  
+    // Attributes
+  public int numberOfPassengers;
+  public double gasolineCapacity;
+  public double consumptionRate;
+  public int averageSpeed;
+  public double distanceTravelled;  
+
+  public CarJava(int NumberOfPassengers, double GasolineCapacity,double ConsumptionRate, int AverageSpeed,
+  double DistanceTravelled){ //constructor
+      numberOfPassengers = NumberOfPassengers;
+      gasolineCapacity = GasolineCapacity;
+      consumptionRate = ConsumptionRate;
+      averageSpeed = AverageSpeed;
+      distanceTravelled = DistanceTravelled;
+  }
+
+  //behaviors of car object
+  //function 1: car can move in certain time period
+
+  public void move(int hours){
+
+    double range = gasolineCapacity / consumptionRate; //maximum distance that car can take with its remained gasoline
+    double distanceToTravel = hours * averageSpeed; // distance = time * velocity
+
+    if(range >= distanceToTravel){
+        double spentGasoline = distanceTravelled * consumptionRate;
+        gasolineCapacity -= spentGasoline;
+        distanceTravelled += distanceToTravel;
+    
+    }else{
+        distanceTravelled += range;
+        gasolineCapacity = 0;
+    }
+  }
+
+  //function 2: see outputs of car object about its move
+  public void report(){
+      System.out.println("distance travelled: " + distanceTravelled);
+      System.out.println("remaining gasoline after travel: " + gasolineCapacity);
+  }
+
+}
