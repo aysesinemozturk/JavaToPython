984522dc988a782646a1b69bf21ef1838c765aac
aysesinemozturk <aysesinemozturk@gmail.com>
2021-07-28 14:28:45 +0300

    Car class in python language is coded. It is same as CarJava class.

---
+++ CarPython.py
@@ -0,0 +1,32 @@
+class CarPython:
+    
+    def __init__(self, NumberOfPassangers, GasolineCapacity,ConsumptionRate, AverageSpeed, DistanceTravelled):
+        # constructor of python
+        self.numberOfPassangers = NumberOfPassangers
+        self.gasolineCapacity = GasolineCapacity
+        self.consumptionRate = ConsumptionRate
+        self.averageSpeed = AverageSpeed
+        self.distanceTravelled = DistanceTravelled
+    
+    # behaviours of car object, function1: car can move
+    
+    def move(self,hours):
+
+        maxTravel = self.gasolineCapacity / self.consumptionRate
+        distanceToTravel = hours * self.averageSpeed
+        
+        if(maxTravel >= distanceToTravel):
+            spentGasoline = self.distanceTravelled * self.consumptionRate
+            self.gasolineCapacity -= spentGasoline
+            self.distanceTravelled += distanceToTravel
+        else:
+            self.distanceTravelled = distanceToTravel
+            self.gasolineCapacity = 0
+    
+    def report(self):
+        print("Distance travelled: ", self.distanceTravelled)
+        print("Remaining gasoline capacity: ", self.gasolineCapacity, "the number of passangers: ", self.numberOfPassangers)
+
+
+
+    
