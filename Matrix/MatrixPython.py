8865cd7dd4b9d756c3654a616e64b6d94f06ed9e
aysesinemozturk <aysesinemozturk@gmail.com>
2021-08-02 13:04:21 +0300

    MatrixPython is revised.

--- Matrix/MatrixPython.py
+++ Matrix/MatrixPython.py
@@ -1,32 +1,39 @@
+from __future__ import print_function
 from array import *
+
 class MatrixPython:
     
     #numberOfRows = 0
     #numberOfColumns = 0
-    #values = [5][5]
+    #values = []
 
-    # A constructor that gets attributes as parameters initializes empty values in the matrix based on its parameters.
-    def __init__(self, NumberOfRows, NumberOfColumns):
-        self.numberOfRows = NumberOfRows
-        self.numberOfColumns = NumberOfColumns
-        values = [self.numberOfRows][self.numberOfColumns]
+   
 
     # A constructor gets again matrix's attributes as parameters, however this time it also takes vectorvalues which was
     # given already in the main method is prepared for the matrix's values. 
     # If vectorvalues length is same as our matrix's dimension, it will take it as values.
     # Otherwise, values of the matrix remains as zero, null.
 
-    def __init__(self, NumberOfRows, NumberOfColumns, vectorValues):
-        if(len(vectorValues) != NumberOfColumns * NumberOfRows):
+    def __init__(self, NumberOfRows, NumberOfColumns, vectorValues = None):
+        
+        self.numberOfRows = NumberOfRows
+        self.numberOfColumns = NumberOfColumns
+        
+        if(vectorValues == None):
+            self.values = [ [ 0 for y in range( self.numberOfColumns ) ] for x in range( self.numberOfRows ) ]
+        
+        elif(len(vectorValues) != NumberOfColumns * NumberOfRows):
+            
             print('the vectorValues array is not suitable for the matrix dimension.')
+            self.values = [ [ 0 for y in range( self.numberOfColumns ) ] for x in range( self.numberOfRows ) ]
+        
         else:
-            self.numberOfRows = NumberOfRows
-            self.numberOfColumns = NumberOfColumns
-            values = [self.numberOfRows-1][self.numberOfColumns-1]
+           
+            self.values = [ [ 0 for y in range( self.numberOfColumns ) ] for x in range( self.numberOfRows ) ]
 
             for i in range (self.numberOfRows):
                 for j in range(self.numberOfColumns):
-                    values[i][j] = vectorValues[i *self.numberOfColumns + j]
+                    self.values[i][j] = vectorValues[i *self.numberOfColumns + j]
 
     # sum up two matrixes which are defined in main method.
     def sum(self, matrix):
@@ -45,11 +52,11 @@ class MatrixPython:
     def report(self):
         
         for i in range(self.numberOfRows):
-            print('|')
+            print('|', end='')
             for j in range(self.numberOfColumns):
-                print(self.values[i][j])
+                print(self.values[i][j],end ='')
                 if(j < self.numberOfColumns - 1):
-                    print('','')
+                    print(',',end ='')
             
             print('|')
 
